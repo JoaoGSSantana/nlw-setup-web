488bd509cdc8aee67c16d2651d1cea0f0c5e94a1
João Gabriel Santana <joao.santana09@outlook.com>
2023-01-20 23:20:17 -0300

    create popver and dialog

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -25,10 +25,14 @@ export function SummaryTable() {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {
-                    summaryDates.map(date => <HabitDay key={date.toString()} />)
+                    summaryDates.map(date => <HabitDay
+                        key={date.toString()}
+                        amount={10}
+                        completed={Math.round(Math.random() * 5)}
+                    />)
                 }
                 {
-                    amountOfDaysToFill > 0 && (Array.from({ length: amountOfDaysToFill }).map((_,index) => {
+                    amountOfDaysToFill > 0 && (Array.from({ length: amountOfDaysToFill }).map((_, index) => {
                         return (
                             <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
                         )
